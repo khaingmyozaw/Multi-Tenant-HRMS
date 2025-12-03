64fe3cf7584b4f9a79c54b4f09461185ea5b0a6b
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-12-03 23:01:58 +0630

    Style: change uuid to id in industry

--- database/migrations/0001_01_01_000000_create_industries_table.php
+++ database/migrations/0001_01_01_000000_create_industries_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('industries', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name');
             $table->string('description')->nullable();
             $table->timestamps();
